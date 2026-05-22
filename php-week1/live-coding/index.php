4cec81fe1c2835d38ab4d13130cbac84a4fad117
vannararin23-sketch <vannararin23@gmail.com>
2026-05-22 09:31:01 +0700

    done

--- php-week1/live-coding/index.php
+++ php-week1/live-coding/index.php
@@ -1,1 +1,9 @@
 <h1>Live Coding</h1>
+<?php
+
+$name = "Rin Vannara";
+$Class = "Web Mobile Application Development (WMAD)";
+
+
+echo "Welcome back, " . $name . "! You are currently enrolled in " . $Class . ".";
+?>
